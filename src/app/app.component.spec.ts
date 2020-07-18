43248274273baba304ad2e3d0b647edaccb0785c
Sunny Gupta <sunny.gupta@primussoft.com>
2020-07-18 13:34:13 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,74 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      // imports: [FormsModule],
+      declarations: [ AppComponent
+       ],
+      //  schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    // component = fixture.debugElement.componentInstance;
+
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  // Test instance of comp. created or not-
+  it('should create the app', async(() => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  }));
+
+  // injector methods handles all operations like add, remove, fetch.
+  it('should use the quoteList from the service', () => {
+    const quoteService = fixture.debugElement.injector.get(QuoteService);
+    fixture.detectChanges();
+    expect(quoteService.getQuote()).toEqual(component.quoteList);
   });
 
-  it(`should have as title 'testingpurpose'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testingpurpose');
+  it('should create hgjh', () => {
+  const re = component.compute(-1);
+  expect(re).toBe(0);
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+});
+
+
+
+
+
+Before; detectchanges; method; call; the; component; is; not; rendered.;
+After; this; method; call; component; is; rendered.;
+
+Now; initialize; the; value in property; of; component; and; check; whether; they; create; new post; or; not.
+it('should create a new post', () => {
+    component.quoteText = 'I love this test';
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testingpurpose!');
+    expect(compiled.innerHTML).toContain('I love this test');
   });
-});
+
+it('should disable the button when textArea is empty', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+it('should enable button when textArea is not empty', () => {
+    component.quoteText = 'I love this test';
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.disabled).toBeFalsy();
+  });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewContainerRef, ViewChild, EmbeddedViewRef, TemplateRef, HostListener} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray, NgForm } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,92 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testingpurpose';
+  // FormGroup
+  // FormControl - single field  formControlName
+  // FormArray - multiple field  formArrayName  formGroupName
+  // setvalue sets value on form control
+  // const control = <FormArray>this.survey.get('sections');
+  //     const control = <FormArray>this.survey.get('sections').controls[j].get('questions');
+  form: FormGroup;
+  empForm:FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.empForm=this.fb.group({
+      employees: this.fb.array([]) ,
+    })
+    //  this.createForm();
+    //  this.form.valueChanges.subscribe(data => console.log(data));
+    this.empForm.get('skills').valueChanges.subscribe(data => console.log(data, 'fvxf'))
+
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      purchaseprice: [''],
+      taxes: '21',
+      purchasepricetaxes: [''],
+      items_value: ['no', Validators.required]
+
+    })
+  }
+
+  calculatePurchasePriceTaxes() {
+    console.log(this.form.value);
+    // return this.form.value.purchasepricetaxes + (+this.form.value.taxes);
+  }
+
+
+ 
+ 
+
+ 
+ 
+  employees(): FormArray {
+    return this.empForm.get("employees") as FormArray
+  }
+ 
+ 
+  newEmployee(): FormGroup {
+    return this.fb.group({
+      firstName: '',
+      lastName: '',
+      skills:this.fb.array([])
+    })
+  }
+ 
+  submit(f){
+
+  }
+  addEmployee() {
+    console.log("Adding a employee");
+    this.employees().push(this.newEmployee());
+  }
+ 
+ 
+  removeEmployee(empIndex:number) {
+    this.employees().removeAt(empIndex);
+  }
+ 
+ 
+  employeeSkills(empIndex:number) : FormArray {
+    return this.employees().at(empIndex).get("skills") as FormArray
+  }
+ 
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+      exp2:''
+    })
+  }
+ 
+  addEmployeeSkill(empIndex:number) {
+    this.employeeSkills(empIndex).push(this.newSkill());
+  }
+ 
+  removeEmployeeSkill(empIndex:number,skillIndex:number) {
+    this.employeeSkills(empIndex).removeAt(skillIndex);
+  }
+    onSubmit() {
+      console.log(this.empForm.value);
+    }
 }
